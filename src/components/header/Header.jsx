a725c7c793d3deec011b637eb2df2ea801a8cc46
Abzall <aipetreu13@gmail.com>
2024-03-14 21:47:43 +0300

    add fonts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from 'react'
-import styles from './header.module.css'
+import React, { useState } from "react";
+import { IoAddOutline } from "react-icons/io5";
+import styles from "./header.module.css";
 
-const Header = ({addTask}) => {
+const Header = ({ addTask }) => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState('')
-    
-    const ChangeInput = (e) => {
-        setInput(e.target.value)
-    }
+  const ChangeInput = (e) => {
+    setInput(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        addTask(input)
-        setInput('')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(input);
+    setInput("");
+  };
 
   return (
     <header className={styles.header}>
-        <h1>Todo List</h1>
-        <form className={styles.taskForm} onSubmit={handleSubmit}>
-            <input type="text" onChange={ChangeInput} value={input} placeholder='Type here...'/>
-            <button>Create</button>
-        </form>
+      <h1>Todo List</h1>
+      <form className={styles.taskForm} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={ChangeInput}
+          value={input}
+          placeholder="Type here..."
+        />
+        <button>
+          Create
+          <IoAddOutline size={25} />
+        </button>
+      </form>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
